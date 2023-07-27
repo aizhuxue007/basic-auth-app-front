afe244cee158ef03e59d502d1024e39a3e744cca
aizhuxue007 <aizhuprogram@gmail.com>
2023-07-27 19:32:10 -0400

    got basic auth working

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,51 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import "./App.css";
 
-import { createClient } from '@supabase/supabase-js'
-import { Auth } from '@supabase/auth-ui-react'
+const supabaseURL = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAPI = import.meta.env.VITE_SUPABASE_API;
 
-import './App.css'
-
-const supabaseURL = import.meta.env.VITE_SUPABASE_URL
-const supabaseAPI = import.meta.env.VITE_SUPABASE_API
-
-console.log(supabaseURL, supabaseAPI)
-const supabase = createClient(supabaseURL, supabaseAPI)
+const supabase = createClient(supabaseURL, supabaseAPI);
 
 function App() {
-  
+  const [ session, setSession ] = useState(null);
 
-  return (
-    <>
-      
-    </>
-  )
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+  console.log(!session)
+  if (!session) {
+    return (
+      <Auth
+        supabaseClient={supabase}
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: {
+                brand: "red",
+                brandAccent: "darkred",
+              },
+            },
+          },
+        }}
+      />
+    );
+  } else {
+    return <div style={{color: 'black'}}>Logged in!</div>;
+  }
 }
 
-export default App
+export default App;
